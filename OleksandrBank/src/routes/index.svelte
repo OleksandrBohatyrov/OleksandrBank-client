5d038fcfc298ce01eaef4d41fee359af8bf1aaf7
synd4772 <doe481157@gmail.com>
2024-09-18 13:11:52 +0300

    WIP

--- OleksandrBank/src/routes/index.svelte
+++ OleksandrBank/src/routes/index.svelte
@@ -40,11 +40,11 @@
 	<title>Sapper project template</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<h1>Tere Tulemast!</h1>
 
 <figure>
 	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Testing 2</figcaption>
+	<figcaption>Tere Tulemast!</figcaption>
 </figure>
 
 <p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
